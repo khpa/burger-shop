9968f15e8954a96ce01d3d45c77b48df60f59102
Christian Paun <christianpaun@gmail.com>
2018-03-06 12:13:56 +0000

    added mobile ver sidemenu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
+class Layout extends Component {
+    
+    state = {
+        showSideDrawer:false
+    }
+
+    sideDrawerClosedHandler = ()=>{
+this.setState({showSideDrawer:false})
+    }
+
+    sideDrawerToggleHandler=()=>{
+        this.setState((prevState)=>{
+            return {showSideDrawer:!this.state.showSideDrawer}
+        });
+    }
+    
+    render () {
+        return (
     <Aux>
-    <Toolbar/>
-    <SideDrawer />    
+    <Toolbar
+    drawerToggleClicked={this.sideDrawerToggleHandler}/>
+    <SideDrawer 
+    open={this.state.showSideDrawer}
+    closed={this.sideDrawerClosedHandler} />    
    
     <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
     </main>
     </Aux>    
-);
+);}
+}
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,27 @@ import navigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props)=> {
+    
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
     return (
-<div className={classes.SideDrawer}>
+<Aux>
+<Backdrop show={props.open} clicked={props.closed}/>    
+<div className={attachedClasses.join(' ')}>
 <div className={classes.Logo}>
     <Logo />
     </div>
     <nav>
     <NavigationItems />
     </nav>
-</div>    
+</div>
+</Aux>    
     );
 }
 
